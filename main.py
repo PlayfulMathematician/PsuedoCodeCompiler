630997833d2f8294a4e8b43499125027e90320f4
PlayfulMathematician <73261021+PlayfulMathematician@users.noreply.github.com>
2022-08-03 20:27:04 -0600

    Update and rename Main.py to main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import single_line
 def main(filename,r):
   with open(filename,r) as f:
     lines=r.readlines()
     for line in lines:
       if str(filter(lambda x:not (x == ' '),line))[0] = "#":
         continue
+      else:
+        single_line.interpret(line)
       
