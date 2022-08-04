5f6e8841353f765a4827b052f3e5998eda3033ef
PlayfulMathematician <73261021+PlayfulMathematician@users.noreply.github.com>
2022-08-04 08:43:35 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import single_line
-import init
+from init import init
 def main(filename,r):
-  with open(filename,r) as f:
-    lines=r.readlines()
-    for line in lines:
-      if str(filter(lambda x:not (x == ' '),line))[0] = "#":
-        continue
-      else:
-        single_line.interpret(line)
-      
+	interpreted = False
+	with open(filename,r) as f:
+		lines=r.readlines()
+    	init(f,filename)
+    	interpreted = True
+    	for line in lines:
+      		if str(filter(lambda x:not (x == ' '),line))[0] = "#":
+        		continue
+      		else:
+        		single_line.interpret(line)
+	if not interpreted:
+    	print("The file you requested doesnt exist")
